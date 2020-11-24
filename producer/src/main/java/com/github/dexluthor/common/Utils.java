bf0d50b8a0abdd5a4ecdd73070b463769f55a8cc
Yevhenii Kozhevin <yevhenii.kozhevin@student.upjs.sk>
2020-11-24 08:58:45 +0100

    @UtilityClass

--- producer/src/main/java/com/github/dexluthor/common/Utils.java
+++ producer/src/main/java/com/github/dexluthor/common/Utils.java
@@ -1,19 +1,21 @@
 package com.github.dexluthor.common;
 
+import lombok.experimental.UtilityClass;
 import lombok.val;
 
 import java.math.BigDecimal;
 
+@UtilityClass
 public class Utils {
-    public static String randomGoal() {
-        String[] values = {
+    public String randomGoal() {
+        final String[] values = {
                 "COFFEE", "LESSON", "MEDICINE", "GIFT", "THANKS", "MEALS", "TOY",
                 "DELIVERY", "BOOK", "GAME", "DOG", "SHELF", "GUN", "POSTER", "FLOWER"
         };
         return values[(int) (Math.random() * values.length)];
     }
 
-    public static BigDecimal randomBill() {
+    public BigDecimal randomBill() {
         val rand = Math.random() * 10_000;
         val point = String.valueOf(rand).indexOf('.');
         val rand2p = (rand + "00").substring(0, point + 3);
@@ -21,8 +23,8 @@ public class Utils {
         return BigDecimal.valueOf(Double.parseDouble(rand2p));
     }
 
-    public static String randomLocation() {
-        String[] locations = {
+    public String randomLocation() {
+        final String[] locations = {
                 "Backer street", "Compton", "Brooklyn", "Odessa", "Kosice", "Bank Go",
                 "Trenchtown", "Saint Martin Bank", "Tatra", "First National", "Big Bank", "Bank Go"
         };
